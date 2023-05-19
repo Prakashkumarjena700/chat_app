b969520c85d2aa1404434f6c3c8b7c3cf91c89d1
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-05-19 14:27:28 +0530

    added server

---
+++ backend/index.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const dotenv = require('dotenv')
+const { chats } = require('./data/data')
+
+const app = express()
+dotenv.config()
+
+app.get('/', (req, res) => {
+    res.send('Api is running')
+})
+
+app.get('/api/chat', (req, res) => {
+    res.send(chats)
+})
+
+app.get('/api/chat/:id', (req, res) => {
+    const singleChat = chats.find((c) => c._id === req.params.id)
+    res.send(singleChat)
+})
+
+const PORT = process.env.port || 4000
+
+app.listen(5000, () => {
+    console.log(`server is running at port ${PORT}`)
+})
