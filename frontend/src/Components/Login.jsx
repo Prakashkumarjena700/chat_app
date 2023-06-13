987b887502fe23f1bd992bb170e9e7c448fe5374
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-06-13 20:01:05 +0530

    signup in frontend

--- frontend/src/Components/Login.jsx
+++ frontend/src/Components/Login.jsx
@@ -1,63 +1,63 @@
-import React, { useState } from 'react'
-import { Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
+// import React, { useState } from 'react'
+// import { Input, InputGroup, InputRightElement, VStack } from '@chakra-ui/react'
 
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  Button,
-  FormHelperText
-} from '@chakra-ui/react'
+// import {
+//   FormControl,
+//   FormLabel,
+//   FormErrorMessage,
+//   Button,
+//   FormHelperText
+// } from '@chakra-ui/react'
 
-export default function Login() {
+// export default function Login() {
 
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [show, setShow] = useState(false)
-
-
-  const submitHandler = () => {
-    console.log(email, password)
-    console.log('Hello')
-  }
-
-  return (
-    <div>
-      <VStack>
-        <FormControl isRequired >
-          <FormLabel>Email</FormLabel>
-          <Input
-            placeholder='Enter your email'
-            onChange={(e) => setEmail(e.target.value)}
-          />
-        </FormControl>
-        <FormControl isRequired >
-          <FormLabel>Password</FormLabel>
-          <InputGroup>
-            <Input
-              type={show ? 'text' : 'password'}
-              placeholder='Enter your password'
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <InputRightElement>
-              <Button onClick={() => setShow(!show)} >
-                {show ? 'hide' : 'show'}
-              </Button>
-            </InputRightElement>
-          </InputGroup>
-        </FormControl>
+//   const [email, setEmail] = useState('')
+//   const [password, setPassword] = useState('')
+//   const [show, setShow] = useState(false)
 
 
-        <Button w='100%' onClick={submitHandler} >Login</Button>
-        <Button w='100%'
-          colorScheme='red'
-          onClick={() => {
-            setEmail('guest@example.com')
-            setPassword('123456')
-          }} >Get Guest User Credentials</Button>
+//   const submitHandler = () => {
+//     console.log(email, password)
+//     console.log('Hello')
+//   }
+
+//   return (
+//     <div>
+//       <VStack>
+//         <FormControl isRequired >
+//           <FormLabel>Email</FormLabel>
+//           <Input
+//             placeholder='Enter your email'
+//             onChange={(e) => setEmail(e.target.value)}
+//           />
+//         </FormControl>
+//         <FormControl isRequired >
+//           <FormLabel>Password</FormLabel>
+//           <InputGroup>
+//             <Input
+//               type={show ? 'text' : 'password'}
+//               placeholder='Enter your password'
+//               onChange={(e) => setPassword(e.target.value)}
+//             />
+//             <InputRightElement>
+//               <Button onClick={() => setShow(!show)} >
+//                 {show ? 'hide' : 'show'}
+//               </Button>
+//             </InputRightElement>
+//           </InputGroup>
+//         </FormControl>
 
 
-      </VStack>
-    </div>
-  )
-}
+//         <Button w='100%' onClick={submitHandler} >Login</Button>
+//         <Button w='100%'
+//           colorScheme='red'
+//           onClick={() => {
+//             setEmail('guest@example.com')
+//             setPassword('123456')
+//           }} >Get Guest User Credentials</Button>
+
+
+//       </VStack>
+//     </div>
+//   )
+// }
