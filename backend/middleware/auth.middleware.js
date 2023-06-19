913ab6e3616b605e21cd4c53d4c9356658f1f8fe
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-06-19 15:13:59 +0530

    added search users

--- backend/middleware/auth.middleware.js
+++ backend/middleware/auth.middleware.js
@@ -4,6 +4,7 @@ const User = require('../models/user.model')
 const auth = (req, res, next) => {
     try {
         console.log('auth middleware')
+        console.log(req)
         next()
     } catch (err) {
         console.log(err)
